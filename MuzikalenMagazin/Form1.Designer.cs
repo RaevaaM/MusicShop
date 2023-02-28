cc7dd3e2fdd7c648782f8ff90211ea94a7091e48
RaevaaM <107184229+RaevaaM@users.noreply.github.com>
2023-02-28 11:10:36 +0200

    upload

---
+++ MuzikalenMagazin/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MuzikalenMagazin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Location = new System.Drawing.Point(435, 477);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Продължете към магазина";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Добре дошли в ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkSalmon;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(425, 176);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 57);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sonata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MuzikalenMagazin.Properties.Resources.backgr;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(734, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Sonata";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MuzikalenMagazin/HomeForm.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace MuzikalenMagazin
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnContacts = new System.Windows.Forms.Button();
+            this.btnGallery = new System.Windows.Forms.Button();
+            this.btnStyles = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.homePanel = new System.Windows.Forms.Panel();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelContacts = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.homePanel.SuspendLayout();
+            this.panelContacts.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Controls.Add(this.btnSignUp);
+            this.panelMenu.Controls.Add(this.btnLogIn);
+            this.panelMenu.Controls.Add(this.btnContacts);
+            this.panelMenu.Controls.Add(this.btnGallery);
+            this.panelMenu.Controls.Add(this.btnStyles);
+            this.panelMenu.Controls.Add(this.btnHome);
+            this.panelMenu.Location = new System.Drawing.Point(471, 1);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 503);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnSignUp.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.Location = new System.Drawing.Point(0, 389);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(200, 114);
+            this.btnSignUp.TabIndex = 5;
+            this.btnSignUp.Text = "Регистрация";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnLogIn.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.Location = new System.Drawing.Point(0, 280);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(200, 115);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Вход";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            // 
+            // btnContacts
+            // 
+            this.btnContacts.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnContacts.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContacts.Location = new System.Drawing.Point(0, 215);
+            this.btnContacts.Name = "btnContacts";
+            this.btnContacts.Size = new System.Drawing.Size(200, 69);
+            this.btnContacts.TabIndex = 3;
+            this.btnContacts.Text = "Контакти";
+            this.btnContacts.UseVisualStyleBackColor = false;
+            // 
+            // btnGallery
+            // 
+            this.btnGallery.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnGallery.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGallery.Location = new System.Drawing.Point(0, 147);
+            this.btnGallery.Name = "btnGallery";
+            this.btnGallery.Size = new System.Drawing.Size(200, 71);
+            this.btnGallery.TabIndex = 2;
+            this.btnGallery.Text = "Галерия";
+            this.btnGallery.UseVisualStyleBackColor = false;
+            // 
+            // btnStyles
+            // 
+            this.btnStyles.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnStyles.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStyles.Location = new System.Drawing.Point(0, 76);
+            this.btnStyles.Name = "btnStyles";
+            this.btnStyles.Size = new System.Drawing.Size(200, 75);
+            this.btnStyles.TabIndex = 1;
+            this.btnStyles.Text = "Стилове";
+            this.btnStyles.UseVisualStyleBackColor = false;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnHome.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(200, 79);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Начало";
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // homePanel
+            // 
+            this.homePanel.BackColor = System.Drawing.Color.PeachPuff;
+            this.homePanel.Controls.Add(this.label1);
+            this.homePanel.Controls.Add(this.label2);
+            this.homePanel.Controls.Add(this.lblHome);
+            this.homePanel.Location = new System.Drawing.Point(723, 12);
+            this.homePanel.Name = "homePanel";
+            this.homePanel.Size = new System.Drawing.Size(476, 503);
+            this.homePanel.TabIndex = 1;
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Monotype Corsiva", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.Location = new System.Drawing.Point(83, 136);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(292, 56);
+            this.lblHome.TabIndex = 0;
+            this.lblHome.Text = "Добре дошли в";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 53);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SONATA!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(444, 82);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "  Тук ще намерите различни \r\nстилове музика и много песни!";
+            // 
+            // panelContacts
+            // 
+            this.panelContacts.BackColor = System.Drawing.Color.PeachPuff;
+            this.panelContacts.Controls.Add(this.label7);
+            this.panelContacts.Controls.Add(this.label6);
+            this.panelContacts.Controls.Add(this.label5);
+            this.panelContacts.Controls.Add(this.label4);
+            this.panelContacts.Controls.Add(this.label3);
+            this.panelContacts.Location = new System.Drawing.Point(-2, 1);
+            this.panelContacts.Name = "panelContacts";
+            this.panelContacts.Size = new System.Drawing.Size(480, 503);
+            this.panelContacts.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(148, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 45);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Контакти";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(462, 68);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Адрес: бул. \"3-ти март\", №125, \r\n                                                " +
+    " гр.София";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 34);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Телефон: 0889256347";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(0, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(298, 34);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Email: sonata@yahoo.com";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(5, 331);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(472, 81);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Свържете се с нас, ако имате някакви въпроси. \r\nЩе се радваме да ни предложите но" +
+    "ви идеи за\r\nнашето приложение!";
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 788);
+            this.Controls.Add(this.homePanel);
+            this.Controls.Add(this.panelContacts);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "HomeForm";
+            this.Text = "HomeForm";
+            this.panelMenu.ResumeLayout(false);
+            this.homePanel.ResumeLayout(false);
+            this.homePanel.PerformLayout();
+            this.panelContacts.ResumeLayout(false);
+            this.panelContacts.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnContacts;
+        private System.Windows.Forms.Button btnGallery;
+        private System.Windows.Forms.Button btnStyles;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Panel homePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Panel panelContacts;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
